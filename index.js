4506ae6d0dc1f61718ebfac607da39cf57a54039
Dan Glick <danjglick@gmail.com>
2022-04-30 05:19:56 -0400

    Add technical skeleton for defense

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-const PLAYER_RADIUS = visualViewport.width / 10
-const BALL_RADIUS = visualViewport.width / 20
+const PLAYER_RADIUS = visualViewport.width / 15
+const BALL_RADIUS = visualViewport.width / 30
 const PIXEL_SHIM = BALL_RADIUS
-const USER_TEAM_COLOR = "CornflowerBlue"
+const USER_COLOR = "CornflowerBlue"
+const ENEMY_COLOR = "Yellow"
 let canvas
 let context
-let players = {
-  user: {
-    offense: {
-      qb: {
-        xPos: visualViewport.width / 2,
-        yPos: visualViewport.height * (2 / 3),
-        radius: PLAYER_RADIUS,
-        color: USER_TEAM_COLOR
-      },
-      wr1: {
-        xPos: visualViewport.width / 4,
-        yPos: visualViewport.height * (2 / 3),
-        radius: PLAYER_RADIUS,
-        color: USER_TEAM_COLOR
-      },
-      wr2: {
-        xPos: visualViewport.width * (3 / 4),
-        yPos: visualViewport.height * (2 / 3),
-        radius: PLAYER_RADIUS,
-        color: USER_TEAM_COLOR
-      }
-    }
+let offense = {
+  qb: {
+    xPos: visualViewport.width / 2,
+    yPos: visualViewport.height * (2 / 3),
+    radius: PLAYER_RADIUS,
+    color: USER_COLOR
+  },
+  wr1: {
+    xPos: visualViewport.width / 4,
+    yPos: visualViewport.height * (2 / 3),
+    radius: PLAYER_RADIUS,
+    color: USER_COLOR
+  },
+  wr2: {
+    xPos: visualViewport.width * (3 / 4),
+    yPos: visualViewport.height * (2 / 3),
+    radius: PLAYER_RADIUS,
+    color: USER_COLOR
   }
 }
+let defense = {
+  dl: {
+    xPos: visualViewport.width / 2,
+    yPos: visualViewport.height * (1 / 2),
+    radius: PLAYER_RADIUS,
+    color: ENEMY_COLOR
+  },
+  lb: {
+    xPos: visualViewport.width / 2,
+    yPos: visualViewport.height * (1 / 3),
+    radius: PLAYER_RADIUS,
+    color: ENEMY_COLOR
+  },
+  s: {
+    xPos: visualViewport.width / 2,
+    yPos: visualViewport.height * (1 / 6),
+    radius: PLAYER_RADIUS,
+    color: ENEMY_COLOR
+  }
+}
+let players = Object.values({ ...offense, ...defense })
 let ball = {
-  xPos: players.user.offense.qb.xPos,
-  yPos: players.user.offense.qb.yPos,
+  xPos: offense.qb.xPos,
+  yPos: offense.qb.yPos,
   xVelocity: 0,
   yVelocity: 0,
   radius: BALL_RADIUS,
@@ -41,6 +59,8 @@ let wr1Path = []
 let isSendingWr1 = false
 let isMovingQb = false
 let isThrowing = false
+let isMovingLb = false
+let onOffense = true
 
 ///// init //////
 
@@ -59,14 +79,9 @@ function gameLoop() {
   context.clearRect(0, 0, canvas.width, canvas.height)
   moveBall()
   if (isMovingQb) {
-    if (wr1Path[0]) {
-      players.user.offense.wr1.xPos = wr1Path[0].xPos
-      players.user.offense.wr1.yPos = wr1Path[0].yPos
-      wr1Path.shift()
-    }
-    if (isClose(ball, players.user.offense.wr1)) {
-      ball.xVelocity = 0
-      ball.yVelocity = 0
+    moveReceivers()
+    if (isClose(ball, offense.wr1)) {
+      handleCatch()
     }
   }
   drawPlayers()
@@ -79,10 +94,16 @@ function handleTouchstart(e) {
     xPos: e.touches[0].clientX,
     yPos: e.touches[0].clientY
   }
-  if (isClose(touch, players.user.offense.wr1)) {
-    isSendingWr1 = true
-  } else if (isClose(touch, players.user.offense.qb)) {
-    isMovingQb = true
+  if (onOffense) {
+    if (isClose(touch, offense.wr1)) {
+      isSendingWr1 = true
+    } else if (isClose(touch, offense.qb)) {
+      isMovingQb = true
+    }
+  } else {
+    if (isClose(touch, defense.lb)) {
+      isMovingLb = true
+    }
   }
 }
 
@@ -92,29 +113,36 @@ function handleTouchmove(e) {
     xPos: e.touches[0].clientX,
     yPos: e.touches[0].clientY
   }
-  if (isSendingWr1) {
-    wr1Path.push(touch)
-  }
-  else if (isMovingQb) {
-    if (
-      Object.keys(priorTouch).length > 0 &&
-      (
-        Math.abs(priorTouch.xPos - touch.xPos) > 20 ||
-        Math.abs(priorTouch.yPos - touch.yPos) > 20
-      )
-    ) {
-      isThrowing = true
-      ball.xVelocity = touch.xPos - priorTouch.xPos
-      ball.yVelocity = touch.yPos - priorTouch.yPos
-    } else {
-      if (isThrowing == false) {
-        players.user.offense.qb.xPos = touch.xPos
-        players.user.offense.qb.yPos = touch.yPos
-      }
+  if (onOffense) {
+    if (isSendingWr1) {
+      wr1Path.push(touch)
     }
-    ball.xPos = touch.xPos
-    ball.yPos = touch.yPos
-    priorTouch = touch
+    else if (isMovingQb) {
+      if (
+        Object.keys(priorTouch).length > 0 &&
+        (
+          Math.abs(priorTouch.xPos - touch.xPos) > 10 ||
+          Math.abs(priorTouch.yPos - touch.yPos) > 10
+        )
+      ) {
+        isThrowing = true
+        ball.xVelocity = touch.xPos - priorTouch.xPos
+        ball.yVelocity = touch.yPos - priorTouch.yPos
+      } else {
+        if (isThrowing == false) {
+          offense.qb.xPos = touch.xPos
+          offense.qb.yPos = touch.yPos
+        }
+      }
+      ball.xPos = touch.xPos
+      ball.yPos = touch.yPos
+      priorTouch = touch
+    }
+  } else {
+    if (isMovingLb) {
+      defense.lb.xPos = touch.xPos
+      defense.lb.yPos = touch.yPos
+    }
   }
 }
 
@@ -124,15 +152,26 @@ function handleTouchend() {
 
 /////// public /////////
 
+function moveBall() {
+  ball.xPos += ball.xVelocity
+  ball.yPos += ball.yVelocity
+}
+
+function moveReceivers() {
+  if (wr1Path[0]) {
+    offense.wr1.xPos = wr1Path[0].xPos
+    offense.wr1.yPos = wr1Path[0].yPos
+    wr1Path.shift()
+  }
+}
+
 function drawPlayers() {
-  players_keys = Object.keys(players.user.offense)
-  for (let i=0; i<players_keys.length; i++) {
-    let player = players.user.offense[players_keys[i]]
+  players.forEach(player => {
     context.beginPath()
     context.arc(player.xPos, player.yPos, player.radius, 0, 2 * Math.PI)
     context.fillStyle = player.color
     context.fill()
-  }
+  })
 }
 
 function drawBall() {
@@ -142,9 +181,9 @@ function drawBall() {
   context.fill()
 }
 
-function moveBall() {
-  ball.xPos += ball.xVelocity
-  ball.yPos += ball.yVelocity
+function handleCatch() {
+  ball.xVelocity = 0
+  ball.yVelocity = 0
 }
 
 //////// private //////////
